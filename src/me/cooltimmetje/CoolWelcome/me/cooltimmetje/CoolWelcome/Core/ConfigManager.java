b8b126a50b241dea1cf512413af3c9ff486700fe
Tim <cooltimmetje@me.com>
2015-01-11 18:41:48 +0100

    Changed the way how the plugin checks if the player is new.

--- src/me/cooltimmetje/CoolWelcome/me/cooltimmetje/CoolWelcome/Core/ConfigManager.java
+++ src/me/cooltimmetje/CoolWelcome/me/cooltimmetje/CoolWelcome/Core/ConfigManager.java
@@ -2,8 +2,6 @@ package me.cooltimmetje.CoolWelcome.me.cooltimmetje.CoolWelcome.Core;
 
 import me.cooltimmetje.CoolWelcome.Main;
 
-import java.util.List;
-
 /**
  * This class has been created on 11-1-2015 at 16:48 by cooltimmetje.
  */
@@ -12,18 +10,11 @@ import java.util.List;
 public class ConfigManager {
 
     public static void onEnableConfig() {
-
         Main.getPlugin().saveDefaultConfig();
-
-        List<String> Players = Main.getPlugin().getConfig().getStringList("data.users");
-        for (String s : Players) {
-            JoinEvent.users.add(s);
-        }
         JoinEvent.amount = Main.getPlugin().getConfig().getInt("data.amount");
     }
 
     public static void writeConfig(){
-        Main.getPlugin().getConfig().set("data.users", JoinEvent.users);
         Main.getPlugin().getConfig().set("data.amount", JoinEvent.amount);
         Main.getPlugin().saveConfig();
     }
